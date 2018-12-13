054c24429830e07b1458fe56fb922b86b2ae1c6c
Nicholas Geary <ncgeary@buffalo.edu>
2018-12-13 06:53:11 -0500

    Update Leaflet.js

--- src/components/Leaflet/Leaflet.js
+++ src/components/Leaflet/Leaflet.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 //https://github.com/fullstackreact/google-maps-react
-import classes from "./Gmap.css";
+
 
 export class Gmap extends Component {
   render() {
