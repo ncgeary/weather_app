6a7664f88742837f5a4cde4e646287b5fd3b1d07
Nicholas Geary <ncgeary@buffalo.edu>
2018-12-16 19:25:56 -0500

    12/16/2018

--- src/components/Leaflet/Leaflet.js
+++ src/components/Leaflet/Leaflet.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import React from 'react';
+import { render } from 'react-dom';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
-const position = [props.lat, props.lon]
+const position = (props.lat,props.lon);
+
+
 const Leaflet = props => (
   <Map center={position} zoom={13}>
+
     <TileLayer
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
     />
-    <Marker position={position}>
+
+    {/*<Marker position={position}>
       <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-    </Marker>
+    </Marker>*/}
+
   </Map>
-)
+);
 
 render(map, document.getElementById('map-container'))
 
