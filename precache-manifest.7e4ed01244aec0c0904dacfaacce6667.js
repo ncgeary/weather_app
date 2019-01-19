722b79f2c4acca9f706c47918a3361eda35058c5
Nicholas Geary <ncgeary@buffalo.edu>
2019-01-19 14:29:50 -0500

    Updates

---
+++ precache-manifest.7e4ed01244aec0c0904dacfaacce6667.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8e50d38204ea6f6971",
+    "url": "/weather_app/static/js/runtime~main.2b8e50d3.js"
+  },
+  {
+    "revision": "e680a01d4f6696fb653c",
+    "url": "/weather_app/static/js/main.e680a01d.chunk.js"
+  },
+  {
+    "revision": "dedf7abc42a50e8e118e",
+    "url": "/weather_app/static/js/1.dedf7abc.chunk.js"
+  },
+  {
+    "revision": "e680a01d4f6696fb653c",
+    "url": "/weather_app/static/css/main.3b70d7f7.chunk.css"
+  },
+  {
+    "revision": "2cb2550ac01c684362670dabe10ba09a",
+    "url": "/weather_app/index.html"
+  }
+];
